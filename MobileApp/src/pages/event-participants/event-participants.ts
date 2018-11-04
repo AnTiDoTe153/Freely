6d4b1eb8c9247ea2fe23388006bc35f11fa9acec
Calin Sulea <cailn.g.sulea@gmail.com>
2018-11-04 10:05:33 +0200

    [dev] Worked on applications page

--- MobileApp/src/pages/event-participants/event-participants.ts
+++ MobileApp/src/pages/event-participants/event-participants.ts
@@ -16,11 +16,48 @@ export class EventParticipantsPage {
 
   private event = this.navParams.data;
 
+  private applicationList: Array<any> =[{
+    name: "Calin",
+    surname: "Sulea",
+    rating: 3.4,
+    status: "applied"
+  },{
+    name: "Flavius",
+    surname: "Filipas",
+    rating: 2.3,
+    status: "accepted"
+  },
+  {
+    name: "Calina",
+    surname: "Antal",
+    rating: 4.2,
+    status: "accepted"
+  },
+  {
+    name: "Serban",
+    surname: "Solomon",
+    rating: 1.0,
+    status: "applied"
+  }];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad EventParticipantsPage');
+  }
+
+  acceptUser(item){
+    console.log(item);
+    item.status = "accepted";
+  }
+
+  getItemColor(item){
+    if(item.status == "applied"){
+      return "third";
+    }else{
+      return "secondary";
+    }
   }
 
 }
